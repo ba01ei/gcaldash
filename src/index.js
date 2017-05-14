b5564ed5e2242fca512894e1621d2444b0fbe558
Bao Lei <blei@uber.com>
2017-05-14 15:45:39 -0400

    minor tweak

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App iframe='iframe' src='https://calendar.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23000000&amp;src=160qm8h89ff0l864k5lvna0pqc%40group.calendar.google.com&amp;color=%238C500B&amp;ctz=America%2FLos_Angeles' />,
+  <App iframe='iframe' src='https://calendar.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=160qm8h89ff0l864k5lvna0pqc%40group.calendar.google.com&amp;color=%238C500B&amp;ctz=America%2FLos_Angeles' />,
   document.getElementById('root')
 );
