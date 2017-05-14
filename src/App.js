e0fc811978842013f006e13ee3b61260d4e5373d
Bao Lei <blei@uber.com>
2017-05-14 03:30:24 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import weather from 'weather-js';
+import logo from './logo.svg';
+import Clock from './clock';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {refreshId: 0};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.updateContent(),
+      5*60*1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  render() {
+    var Iframe=this.props.iframe;
+    return (
+      <div className="App">
+        <div style={{width: 768, height: 524, display: 'table'}} >
+          <div style={{display: 'table-cell', verticalAlign: 'middle'}}>
+            <Clock />
+          </div>
+        </div>
+        <Iframe src={this.props.src+'&id='+this.state.refreshId} height={500} width={768} style={{borderWidth:0}} />
+      </div>
+    );
+  }
+
+  updateContent() {
+    this.setState({
+      refreshId: Math.random()
+    });
+  }
+}
+
+export default App;
